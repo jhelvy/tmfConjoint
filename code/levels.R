ec1527dd856b7a8e7d51a29d75817843b82d00ad
John Paul Helveston <john.helveston@gmail.com>
2019-07-09 11:31:03 -0400

    minor tweaks

--- code/levels.R
+++ code/levels.R
@@ -10,13 +10,12 @@ source(file.path('code', 'functions.R'))
 # -----------------------------------------------------------------------------
 
 # Attributes & Levels
-mode_price <- c('Bus ($1)', 'Bus ($3)', 'Bus ($5)', 'Bus ($9)',
-              'Car (Free Parking)', 'Car ($5 Parking)', 'Car ($10 Parking)',
-              'Car ($15 Parking)', 'Car ($20 Parking)',
-              'Scooter / Bicycle ($1.50)', 'Scooter / Bicycle ($3.00)',
-              'Scooter / Bicycle ($5.00)', 'Uber / Lyft ($10)',
-              'Uber / Lyft ($15)', 'Uber / Lyft ($20)',
-              'Uber / Lyft ($30)', 'Walking ')
+mode_price <- c(
+    'Bus ($1)', 'Bus ($3)', 'Bus ($5)', 'Bus ($9)', # Bus fare
+    'Car ($5)', 'Car ($10)', 'Car ($15)', 'Car ($20)', 'Car ($30)', # Parking + Gas
+    'Uber / Lyft ($10)', 'Uber / Lyft ($15)', 'Uber / Lyft ($20)', 'Uber / Lyft ($30)', # Travel fare
+    'Scooter / Bicycle ($1.50)', 'Scooter / Bicycle ($3.00)', 'Scooter / Bicycle ($5.00)', # Rental fare
+    'Walk')
 leg_travel_time     <- c(10, 15, 20, 30, 45, 60) # Minutes
 leg_travel_time_unc <- c(0, 10, 20, 30, 40) # Percentage of time
 num_transfers       <- c(0, 1, 2, 3)
