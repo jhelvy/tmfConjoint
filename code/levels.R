656172790b3d53e2b21425dbdc4f2bb7e3995b07
John Helveston <john.helveston@gmail.com>
2019-07-13 10:19:28 -0400

    Started DOE plots

--- code/levels.R
+++ code/levels.R
@@ -1,11 +1,4 @@
-# -----------------------------------------------------------------------------
-# Setup
-
-library(tidyverse)
-library(rvest)
-library(mapsapi)
-options(dplyr.width = Inf) # Option to preview all columns in a data frame
-source(file.path('code', 'functions.R'))
+library(ggplot2)
 
 # -----------------------------------------------------------------------------
 # Mode-specific designs
@@ -64,7 +57,7 @@ public <- expand.grid(
 # -----------------------------------------------------------------------------
 # Full factorial
 
-ff <- expand.grid(
+ff1 <- expand.grid(
     car_price_drive  = c(10, 15, 20, 30), # USD $ Parking + Gas
     car_price_uber   = c(10, 15, 20, 30), # Ride Fare
     car_rideTime     = c(1, 2, 3, 4), # Level (defined by map)
@@ -77,3 +70,37 @@ ff <- expand.grid(
                          'Scooter / Bike ($3.00)', 'Scooter / Bike ($5.00)'),
     bus_lastMileTime = c(5, 10) # Minutes
 )
+
+ff2 <- expand.grid(
+    car_price_drive  = c(10, 20, 30), # USD $ Parking + Gas
+    car_price_uber   = c(10, 20, 30), # Ride Fare
+    car_rideTime     = c(1, 2, 3), # Level determined by map
+    car_rideTimeUnc  = c(0, 10, 20), # Percentage of time
+    bus_price        = c(2, 5, 8), # USD $ ticket fare
+    bus_rideTime     = c(1, 2, 3), # Level determined by map
+    bus_rideTimeUnc  = c(0, 10, 20), # Percentage of time
+    bus_numTransfers = c(0, 1, 2),
+    bus_lastMileTime = c(5, 10) # Minutes
+)
+
+# -----------------------------------------------------------------------------
+# Make trip maps 
+
+row <- ff2[1,]
+
+getDf <- function(row) {
+    x = c(0, 1)
+    if (row$bus_numTransfers == 1) {
+        x = c(0, 0.5, 1)
+    } else if (row$bus_numTransfers == 2) {
+        x = c(0, 0.33, 0.66, 1)
+    } 
+    return(data.frame(x = x, y = rep(1, length(x))))
+}
+
+ggplot(getDf(row), 
+    aes(x = x, y = y)) +
+    geom_point(size=2) + 
+    geom_line() + 
+    theme_bw()
+    
