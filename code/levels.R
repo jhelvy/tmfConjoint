11b3add4f3fea7f1f4f49a254a733e293c14d5c9
John Paul Helveston <john.helveston@gmail.com>
2019-07-15 17:30:26 -0400

    still struggling to get plots working

--- code/levels.R
+++ code/levels.R
@@ -98,25 +98,7 @@ ff3 <- expand.grid(
 )
 
 # -----------------------------------------------------------------------------
-# Make trip maps
-
-getPlotDf <- function(x) {return(data.frame(x = x, y = rep(1, length(x))))}
-
-getPlotX <- function(row) {
-    plotDf = getPlotDf0()
-    plotDf = data.frame(
-        type = c('car', 'car', 'uber', 'uber'),
-        x    = c(0, 1, 0, 1),
-        y    = c(1, 1, 1, 1)
-    )
-    if (row$bus_numTransfers == 1) {
-        x = c(0, 0.5, 1)
-    } else if (row$bus_numTransfers == 2) {
-        x = c(0, 0.33, 0.66, 1)
-    }
-    plotDf = data.frame(x = x, y = rep(1, length(x)))
-    return(plotDf)
-}
+# Translate full factorial into design for plotting
 
 carTimes <- data.frame(
     car_rideTime = seq(3),
@@ -149,9 +131,36 @@ design <- ff3 %>%
     arrange(id) %>%
     spread(var, val)
 
+# -----------------------------------------------------------------------------
+# Make trip maps
+
+getPlotDf <- function(x) {return(data.frame(x = x, y = rep(1, length(x))))}
+
+getPlotX <- function(row) {
+    carDf = getPlotDf(x=c(0, 1))
+    uberDf = getPlotDf(x=c(0, 1))
+    busDf = getPlotDf(x=seq(0, 1, length.out=(unique(row$numTransfers) + 2)))
+    plotDf = getPlotDf0()
+    plotDf = data.frame(
+        type = c('car', 'car', 'uber', 'uber'),
+        x    = c(0, 1, 0, 1),
+        y    = c(1, 1, 1, 1)
+    )
+    if (row$bus_numTransfers == 1) {
+        x = c(0, 0.5, 1)
+    } else if (row$bus_numTransfers == 2) {
+        x = c(0, 0.33, 0.66, 1)
+    }
+    plotDf = data.frame(x = x, y = rep(1, length(x)))
+    return(plotDf)
+}
+
+
+
 i <- 13123
 row  <- filter(design, id == i)
 plotDf <- getPlotDf(row)
+
 carPlot <- ggplot(plotDf,
     aes(x = x, y = y)) +
     geom_point(size=2) +
@@ -170,9 +179,21 @@ busPlot <- ggplot(plotDf,
 
 
 
-ggplot(plotDf,
+ggplot(getPlotDf(x=c(0, 1)),
     aes(x = x, y = y)) +
     geom_point(size=2) +
     geom_line() +
     theme_void() +
-    facet_wrap(~type)
+    annotate("text", x = 0.5, y = 0.8, label = row[which(row$type == 'car'),]$time) +
+    annotate("text", x = 0.5, y = 0.6, label = paste('$', row[which(row$type == 'car'),]$price, ' parking + gas', sep='')) +
+    annotate("text", x = 0.5, y = 10, label = '')
+
+
+
+
+
+
+
+
+
+
