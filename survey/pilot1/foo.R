064aa6a79d13238f630f31352754922eea3961cc
John Paul Helveston <john.helveston@gmail.com>
2019-08-06 14:44:21 -0400

    Update foo.R

--- survey/pilot1/foo.R
+++ survey/pilot1/foo.R
@@ -4,8 +4,19 @@ library(ggplot2)
 # -----------------------------------------------------------------------------
 # Functions for making trip images
 
+addWaitTimes <- function(labels, trip) {
+    if (trip$taxiWaitTime > 0) {
+        
+        if ('Uber/Lyft' %in% labels) {
+            gsub('Uber/Lyft', paste('Uber/Lyft'), labels)
+            
+        }
+
+    }
+}
+
 getLabels <- function(trip) {
-    modeLabels <- strsplit(trip$trip, '\\|')[[1]]
+    modeLabels <- trimws(strsplit(trip$trip, '\\|')[[1]])
     names(modeLabels) <- seq(1, 2*length(modeLabels), by=2)
     if (trip$numLegs > 1) {
         transferLabels <- rep('Transfer', trip$numLegs-1)
@@ -15,14 +26,16 @@ getLabels <- function(trip) {
     } else {
         labels <- modeLabels
     }
-    return(rev(c('Start', labels, 'End')))
+    labels <- c('Start', labels, 'End')
+    labels <- addWaitTimes(labels, trip)
+    return(labels)
 }
 
 getPlotDf <- function(trip) {
     labels <- getLabels(trip)
     plotDf <- data.table(
         x     = 0,
-        y     = seq(0, 1, length.out = length(labels)),
+        y     = seq(0, -1, length.out = length(labels)),
         label = labels)
     plotDf[, type := ifelse(label %in% c('Start', 'Transfer', 'End'),
                             'Node', 'Edge')]
@@ -31,13 +44,10 @@ getPlotDf <- function(trip) {
 
 makePlot <- function(trip) {
     p <- ggplot(data = getPlotDf(trip), aes(x = x, y = y)) +
-        geom_point(size=5, aes(color=type)) +
-        scale_color_manual(values=c("#FFFFFF", "#000000")) +
         geom_line() +
-        geom_label(aes(x = x + 1, y = y, label = label, fill = type)) +
+        geom_label(aes(x = x, y = y, label = label, fill = type)) +
         scale_fill_manual(values=c("#FFFFFF", "#E0E0E0")) +
-        # annotate("text", x = 8, y = 0, label = "") +
-        scale_x_continuous(limits = c(-0.5, 2)) +
+        scale_x_continuous(limits = c(-0.5, 1)) +
         theme_void() +
         theme(
             legend.position = "none",
@@ -52,9 +62,15 @@ doeFilePath <- 'https://raw.githubusercontent.com/jhelvy/tmfConjoint/master/surv
 doe <- fread(doeFilePath)
 
 
+respondentID <- 1
+if (respondentID > max(doe$respID)) {
+    respondentID <- respondentID %% max(doe$respID)
+}
 
+# Filter out the respondent
+doe <- doe[respID == respondentID]
+trip1 <- doe[(altID == 1) & (qID == 1)]
+trip2 <- doe[(altID == 2) & (qID == 1)]
+trip3 <- doe[(altID == 3) & (qID == 1)]
 
-
-
-
-
+makePlot(trip1)
