db6a3f9806834586699ca78732c3a473a96e8d93
John Paul Helveston <john.helveston@gmail.com>
2019-08-18 13:40:45 -0400

    Initial pilot 3

---
+++ survey/pilot3/doe.R
@@ -0,0 +1,129 @@
+library(here)
+source(here::here('survey', 'pilot3', 'functions.R'))
+
+# -----------------------------------------------------------------------------
+# Main DOE construction - randomized, stratified by number of trips and modes
+
+# Get unique trip combinations
+car  <- c('Car', 'Car\n(Express Lane)')
+uber <- paste(rep('Uber/Lyft-', 3), c(2, 5, 10), rep(' min wait', 3), sep='')
+taxi <- paste(rep('Taxi-', 3), c(2, 5, 10), rep(' min wait', 3), sep='')
+bus  <- paste(rep('Bus-', 3), c(2, 5, 10), rep(' min wait', 3), sep='')
+walk <- paste(rep('Walk-', 3), c(2, 5, 10), rep(' min', 3), sep='')
+
+trips1 <- data.frame(trip = c(car, uber, taxi, bus))
+trips2 <- expand.grid(
+    leg1 = trips1$trip,
+    leg2 = bus) %>%
+    mutate(trip = paste(leg1, leg2, sep='|'))%>%
+    select(trip)
+trips3 <- expand.grid(
+    leg1 = trips1$trip,
+    leg2 = c(bus),
+    leg3 = c(uber, taxi, bus)) %>%
+    mutate(trip = paste(leg1, leg2, leg3, sep='|') )%>%
+    select(trip)
+# Add taxi options for trips3
+taxi3 <- trips3 %>%
+    filter(str_detect(trip, 'Uber')) %>%
+    mutate(trip = str_replace_all(trip, 'Uber/Lyft', 'Taxi'))
+trips3 <- rbind(trips3, taxi3)
+
+# Set general attributes
+atts <- list(
+    price         = c(10, 15, 20, 25, 30), # USD $
+    travelTime    = c(20, 30, 40, 50, 60), # Minutes for time in motion
+    tripTimeUnc   = c(0.05, 0.1, 0.2), # Plus/minus percentage of totalTripTime
+    walkTimeStart = c(2, 5, 10), # Minutes
+    walkTimeEnd   = c(2, 5, 10), # Minutes
+)
+
+# Make ff for trips of length 1
+ff1 <- makeFullFactorial(trips1$trip, atts)
+ff1$rowID <- paste('1', seq(nrow(ff1)), sep='-')
+
+# Make ff for trips of length 2
+ff2 <- makeFullFactorial(trips2$trip, atts)
+ff2$rowID <- paste('2', seq(nrow(ff2)), sep='-')
+
+# Make ff for trips of length 3
+ff3 <- makeFullFactorial(trips3$trip, atts)
+ff3$rowID <- paste('3', seq(nrow(ff3)), sep='-')
+
+# Sample from ff1, ff2, and ff3 for even numbers of trip legs and modes
+N <- 6000
+sampleIDs1 <- getSampleIDs(ff1, N)
+sampleIDs2 <- getSampleIDs(ff2, N)
+sampleIDs3 <- getSampleIDs(ff3, N)
+doe1 <- ff1[unlist(sampleIDs1),]
+doe2 <- ff2[unlist(sampleIDs2),]
+doe3 <- ff3[unlist(sampleIDs3),]
+doe <- rbind(doe1, doe2, doe3)
+
+# Randomize alternatives
+rows <- sample(x=seq(nrow(doe)), size=nrow(doe), replace=F)
+doe  <- doe[rows,]
+
+# -----------------------------------------------------------------------------
+# Translate full factorial into design for plotting
+
+# Compute trip time range
+doe <- doe %>%
+    mutate(
+        nBus  = str_count(trip, 'Bus'),
+        nTaxi = str_count(trip, 'Taxi') + str_count(trip, 'Uber'),
+        totalWalkTime = walkTimeStart + walkTimeEnd,
+        totalWaitTime = taxiWaitTime*nTaxi + busWaitTime*nBus,
+        totalTripTime = totalWalkTime + totalWaitTime + travelTime,
+        tripTimeMin = round(totalTripTime*(1 - tripTimeUnc)),
+        tripTimeMax = round(totalTripTime*(1 + tripTimeUnc)),
+        tripTimeRange  = paste(
+            tripTimeMin, '-', tripTimeMax, 'minutes', sep=' '))
+
+# Make sure no 2 same alts appear in one question, and set meta data
+nAltsPerQ <- 3 # Number of alternatives per question
+nQPerResp <- 6 # Number of questions per respondent
+doe       <- removeDoubleAlts(doe, nAltsPerQ, nQPerResp)
+
+# Save design
+write_csv(doe, here::here('survey', 'pilot2', 'doe', 'doe.csv'))
+
+# -----------------------------------------------------------------------------
+# Read in the doe and convert it to individual trips
+
+doe <- read_csv(here::here('survey', 'pilot2', 'doe', 'doe.csv'))
+
+# Create trips
+tripDfList <- list()
+for (i in 1:nrow(doe)) {
+    tripDfList[[i]] <- getTripDf(doe[i,])
+}
+
+# Save trip list
+saveRDS(tripDfList, here::here('survey', 'pilot2', 'doe', 'tripDfList.Rds'))
+
+# -----------------------------------------------------------------------------
+# Read in trip list and save all trips for each respondent as a csv file
+
+tripDfList <- readRDS(here::here('survey', 'pilot2', 'doe', 'tripDfList.Rds'))
+
+# Save each trip
+respID <- 1
+index <- 2
+temp <- list(tripDfList[[1]])
+for (i in 2:length(tripDfList)) {
+    trip <- tripDfList[[i]]
+    if (trip$respID[1] == respID) {
+        temp[[index]] <- trip
+        index <- index + 1
+    } else {
+        # Save the tripDf
+        tripDf <- do.call(rbind, temp)
+        write_csv(tripDf, here::here('survey', 'pilot2', 'doe', 'trips',
+                                     paste(respID, '.csv', sep='')))
+        # Start a new temp list
+        respID <- trip$respID[1]
+        index <- 2
+        temp <- list(trip)
+    }
+}
---
+++ survey/pilot3/functions.R
@@ -0,0 +1,186 @@
+library(tidyverse)
+library(ggrepel)
+library(data.table)
+options(dplyr.width = Inf) # Option to preview all columns in a data frame
+
+# -----------------------------------------------------------------------------
+# Functions making the DOE
+
+removeIllogicalTrips <- function(ff) {
+    design <- ff %>%
+        mutate(
+            numLegs      = str_count(trip, '\\|') + 1,
+            firstLegMode = word(trip, sep='\\|', 1),
+            lastLegMode  = word(trip, sep='\\|', numLegs),
+            carInTrip    = str_detect(trip, 'Car'),
+            busInTrip    = str_detect(trip, 'Bus'),
+            taxiInTrip   = str_detect(trip, 'Taxi') | str_detect(trip, 'Uber'),
+            # You only walk at the start or end of a bus trip
+            walkTimeStart = ifelse(
+                (busInTrip == F) | (firstLegMode != 'Bus'), 0, walkTimeStart),
+            walkTimeEnd = ifelse(
+                (busInTrip == F) | (lastLegMode != 'Bus'), 0, walkTimeEnd),
+            # You only wait on the bus if it's in the mode set and
+            busWaitTime = ifelse(
+                (busInTrip == F), 0, busWaitTime),
+            # You only wait on a taxi if it's in the mode set
+            taxiWaitTime = ifelse(
+                (taxiInTrip == F), 0, taxiWaitTime)
+        ) %>% distinct()
+    return(design)
+}
+
+makeFullFactorial <- function(trip, atts) {
+    atts$trip <- trip
+    ff <- expand.grid(atts)
+    return(removeIllogicalTrips(ff))
+}
+
+getSampleIDs <- function(ff, numSamples) {
+    car  <- sample(x=which(ff$carInTrip),  size=numSamples, replace=T)
+    taxi <- sample(x=which(ff$taxiInTrip), size=numSamples, replace=T)
+    bus  <- sample(x=which(ff$busInTrip),  size=numSamples, replace=T)
+    return(list(car=car, taxi=taxi, bus=bus))
+}
+
+addMetaData <- function(doe, nAltsPerQ, nQPerResp) {
+    nRowsPerResp   <- nAltsPerQ * nQPerResp
+    nResp          <- nrow(doe) / nRowsPerResp # Number of respondents
+    doe$respID     <- rep(seq(nResp), each=nRowsPerResp)
+    doe$qID        <- rep(rep(seq(nQPerResp), each=nAltsPerQ), nResp)
+    doe$altID      <- rep(seq(nAltsPerQ), nResp*nQPerResp)
+    doe$obsID      <- rep(seq(nResp * nQPerResp), each=nAltsPerQ)
+    row.names(doe) <- seq(nrow(doe))
+    return(doe)
+}
+
+getUniqueAltCounts <- function(doe) {
+    doe <- doe %>%
+        group_by(obsID) %>%
+        mutate(numUnique = n_distinct(rowID)) %>%
+        ungroup()
+    return(doe)
+}
+
+removeDoubleAlts <- function(doe, nAltsPerQ, nQPerResp) {
+    doe <- addMetaData(doe, nAltsPerQ, nQPerResp)
+    doe <- getUniqueAltCounts(doe)
+    doubleRows <- which(doe$numUnique != 3)
+    while (length(doubleRows) != 0) {
+        newRows <- sample(x=seq(nrow(doe)), size=length(doubleRows), replace=F)
+        doe[doubleRows,] <- doe[newRows,]
+        doe <- addMetaData(doe, nAltsPerQ, nQPerResp)
+        doe <- getUniqueAltCounts(doe)
+        doubleRows <- which(doe$numUnique != 3)
+    }
+    doe <- doe %>% select(-rowID, -numUnique)
+    return(doe)
+}
+
+# -----------------------------------------------------------------------------
+# Functions for making the trips
+
+addWalking <- function(trip, row) {
+    if (row$walkTimeStart > 0) {
+        label <- paste('Walk\n(', row$walkTimeStart, ' min)', sep='')
+        trip  <- c(label, 'Transfer', trip)
+    }
+    if (row$walkTimeEnd > 0) {
+        label <- paste('Walk\n(', row$walkTimeEnd, ' min)', sep='')
+        trip  <- c(trip, 'Transfer', label)
+    }
+    return(trip)
+}
+
+addWaitTimes <- function(trip, row) {
+    if (row$taxiWaitTime > 0) {
+        trip <- addModeWaitTime(trip, 'Taxi', row$taxiWaitTime)
+        trip <- addModeWaitTime(trip, 'Uber/Lyft', row$taxiWaitTime)
+    }
+    if (row$busWaitTime > 0) {
+        trip <- addModeWaitTime(trip, 'Bus', row$busWaitTime)
+    }
+    return(trip)
+}
+
+addModeWaitTime <- function(trip, mode, waitTime) {
+    if (mode %in% trip) {
+        modeID <- which(trip == mode) - 1
+        trip[modeID] <- paste(trip[modeID], '\n(', waitTime,
+                              ' min wait)', sep='')
+    }
+    return(trip)
+}
+
+makeTripVector <- function(row) {
+    trip <- str_replace_all(row$trip, '\\|', '|Transfer|')
+    trip <- str_split(trip, '\\|')[[1]]
+    trip <- addWalking(trip, row)
+    trip <- c('Start', trip, 'End')
+    trip <- addWaitTimes(trip, row)
+    return(trip)
+}
+
+getTripNodes <- function(trip, row) {
+    numNodes <- length(trip)
+    if (row$walkTimeStart > 0) {numNodes <- numNodes - 2}
+    if (row$walkTimeEnd > 0) {numNodes <- numNodes - 2}
+    y   <- seq(0, -1, length.out = numNodes)
+    gap <- y[1] - y[2]
+    if (row$walkTimeStart > 0) {
+        node1 <- y[1] + gap/2
+        node2 <- y[1] + gap
+        y <- c(node2, node1, y) - gap
+    }
+    if (row$walkTimeEnd > 0) {
+        node1 <- y[length(y)] - gap/2
+        node2 <- y[length(y)] - gap
+        y <- c(y, node1, node2)
+    }
+    return(y)
+}
+
+addPlotStats <- function(tripDf, row) {
+    # Compute where to put nodes
+    node = rep(0, nrow(tripDf))
+    node[which(str_detect(tripDf$label, 'Transfer'))] <- 1
+    # Compute where to draw the solid line
+    line = rep(0, nrow(tripDf))
+    if (row$walkTimeStart > 0) {
+        line[3] <- 1
+    } else {
+        line[1] <- 1
+    }
+    if (row$walkTimeEnd > 0) {
+        line[nrow(tripDf)-2] <- 1
+    } else {
+        line[nrow(tripDf)] <- 1
+    }
+    # Compute which type of label to print
+    labelType = rep('Transit', nrow(tripDf))
+    labelType[which(str_detect(tripDf$label, 'Transfer'))] <- 'Node'
+    labelType[c(1, nrow(tripDf))] <- 'Terminal'
+    # Add variables to data frame
+    tripDf$node <- node
+    tripDf$line <- line
+    tripDf$labelType <- labelType
+    return(tripDf)
+}
+
+getTripDf <- function(row) {
+    trip   <- makeTripVector(row)
+    nodes  <- getTripNodes(trip, row)
+    tripDf <- tibble(
+        x     = 0,
+        y     = nodes,
+        label = trip) %>%
+        addPlotStats(row) %>%
+        mutate(
+            respID = row$respID,
+            qID    = row$qID,
+            altID  = row$altID,
+            obsID  = row$obsID,
+            price  = row$price,
+            timeRange = row$tripTimeRange)
+    return(tripDf)
+}
