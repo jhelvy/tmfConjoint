06119e408b52ec4fa994ac06409ae7b3e5c73bc3
John Paul Helveston <john.helveston@gmail.com>
2019-09-29 20:05:54 -0400

    Added tripExample for explanation page

--- survey/pilot7/foo.R
+++ survey/pilot7/foo.R
@@ -3,23 +3,17 @@ setwd('/Users/jhelvy/gh/tmfConjoint/')
 library(here)
 source(here::here('survey', 'pilot7', 'functions.R'))
 
-# -----------------------------------------------------------------------------
-
 rootPath <- "https://raw.githubusercontent.com/jhelvy/tmfConjoint/master/survey/pilot7/survey/"
-if (hasCar == 1) {
-    tripDfPath <- paste(rootPath, 'tripDfsAll.csv', sep = '')
-} else {
-    tripDfPath <- paste(rootPath, 'tripDfListNoCar.csv', sep = '')
-}
-respID <- sample(seq(max(fread(tripDfPath)$respID)), 1)
+tripDfPath <- paste(rootPath, 'tripDfListNoCar.csv', sep = '')
+respondentID <- 7
 plotFuncsPath <- paste(rootPath, "plotFuncs.R", sep='')
 
-# Filter out the trips
+source(plotFuncsPath)
+doe <- fread(tripDfPath)[respID == respondentID]
+# Filter out the alternatives
 trip1 <- doe[(altID == 1) & (qID == 1)]
 trip2 <- doe[(altID == 2) & (qID == 1)]
 trip3 <- doe[(altID == 3) & (qID == 1)]
-
-source(here::here('survey', 'pilot7', 'survey', 'plotFuncs.R'))
 
 makePlot(trip2)
 
